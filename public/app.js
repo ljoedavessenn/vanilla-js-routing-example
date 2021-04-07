d9f47faedf5ccc3caf525c554f5bab6999acf99a
Mulry Joe Davessenn Louis-Jeune <ljoedavessenn@gmail.com>
2021-04-07 18:28:57 +0000

    first commit

---
+++ public/app.js
@@ -0,0 +1,62 @@
+//Declare the variables for home, about & contact html pages
+let home = '';
+let about = '';
+let contact = '';
+
+
+let routes;
+
+//Get the Element with the Id 'root'
+const rootDiv = document.getElementById('root');
+
+/**
+ *
+ * @param {String} page - Represents the page information that needs to be retrieved
+ * @returns {String} resHtml - The Page's HTML is returned from the async invocation
+ */
+
+const loadPage = async (page) => {
+    const response = await fetch(page);
+    const resHtml = await response.text();
+    return resHtml;
+};
+
+/**
+ * The Async function loads all HTML to the variables 'home', 'about' & 'contact'
+ */
+const loadAllPages = async () => {
+    home = await loadPage('./home.html');
+    about = await loadPage('./about.html');
+    contact = await loadPage('./contact.html');
+};
+
+
+/**
+ * The Main Function is an async function that first loads All Page HTML to the variables
+ * Once the variables are loaded with the contents, then they are assigned to the 'routes' variable
+ */
+
+const main = async () => {
+    await loadAllPages();
+    routes = {
+        '/': home,
+        '/contact': contact,
+        '/about': about,
+    };
+    rootDiv.innerHTML = routes[window.location.pathname];
+};
+
+// Invoke the Main function
+main();
+
+const onNavClick = (pathname) => {
+    window.history.pushState({}, pathname, window.location.origin + pathname); 
+    rootDiv.innerHTML = routes[pathname];
+};
+
+/**
+ * The Function is invoked when the window.history changes
+ */
+window.onpopstate = () => {
+    rootDiv.innerHTML = routes[window.location.pathname];
+};
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.use(express.static('public'));
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
